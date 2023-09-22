1191092479a6402ae2b1a55107258847b45332c6
Eduardo Barijan <eduardo.barijan@devsquad.com>
2023-09-21 21:21:43 -0300

    response test

---
+++ tests/Unit/ResponseTest.php
@@ -0,0 +1,11 @@
+<?php
+
+test('a Response object can be created', function () {
+    // ACT
+    $response = new Response('{"foo":"bar"}', 200);
+
+    // ASSERT
+    expect($response->getStatusCode())->tobeInt()->toBe(200)
+        ->and($response->getBody())
+        ->toMatchJson(['foo' => 'bar']);
+});
