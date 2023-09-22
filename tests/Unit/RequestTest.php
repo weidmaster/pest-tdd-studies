756fdd6dcda308098537ed8f440e5d8108b5bdc8
Eduardo Barijan <eduardo.barijan@devsquad.com>
2023-09-21 22:13:13 -0300

    request test

---
+++ tests/Unit/RequestTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Request;
+
+it('creates a correctly formed GET Request object', function () {
+    // ACT
+    $request = Request::create(
+        'GET',
+        '/some/path?black=white&day=night',
+        [
+            'CONTENT_TYPE' => 'application/json',
+            'Accept' => 'application/json'
+        ],
+        ''
+    );
+
+    // ASSERT
+    expect($request->getQueryParams())
+        ->toMatchArray(['black' => 'white', 'day' => 'night'])
+        ->and($request->getPath())
+        ->toBe('/some/path')
+        ->and($request->getMethod())
+        ->toBe('GET');
+});
