84ccfaa6e2c300028eca51341da3b56beb7554f6
Eduardo Barijan <eduardo.barijan@devsquad.com>
2023-10-25 23:17:26 -0300

    testing routing errors

--- tests/Unit/RouterTest.php
+++ tests/Unit/RouterTest.php
@@ -27,11 +27,55 @@ it('returns a 200 Response object if a valid route exists', function () {
     // ASSERT
     expect($response)
         ->toBeInstanceOf(Response::class)
-        ->and($response->getStatusCode())->toBe(200);
+        ->and($response->getStatusCode())->toBe(Response::HTTP_OK);
 });
 
 it('returns a 404 Response object if a route does not exist', function () {
-})->todo();
+    // ARRANGE
+    $request = Request::create('GET', '/foo');
+    $handler = fn () => new Response();
+
+    $routeHandlerResolver = Mockery::mock(RouteHandlerResolver::class);
+    $routeHandlerResolver->shouldReceive('resolve')
+        ->andReturn($handler);
+
+    $router = new Router($routeHandlerResolver);
+
+
+    $router->setRoutes([
+        ['GET', '/bar', $handler]
+    ]);
+
+    // ACT
+    $response = $router->dispatch($request);
+
+    // ASSERT
+    expect($response)
+        ->toBeInstanceOf(Response::class)
+        ->and($response->getStatusCode())->toBe(Response::HTTP_NOT_FOUND);
+});
 
 it('returns a 405 Response object if a not allowed method is used', function () {
-})->todo();
+    // ARRANGE
+    $request = Request::create('GET', '/foo');
+    $handler = fn () => new Response();
+
+    $routeHandlerResolver = Mockery::mock(RouteHandlerResolver::class);
+    $routeHandlerResolver->shouldReceive('resolve')
+        ->andReturn($handler);
+
+    $router = new Router($routeHandlerResolver);
+
+
+    $router->setRoutes([
+        ['POST', '/foo', $handler]
+    ]);
+
+    // ACT
+    $response = $router->dispatch($request);
+
+    // ASSERT
+    expect($response)
+        ->toBeInstanceOf(Response::class)
+        ->and($response->getStatusCode())->toBe(Response::HTTP_METHOD_NOT_ALLOWED);
+});
